6ef6bdafee2fa9792e3b82526d0268cb3d762122
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-08-22 15:46:50 -0700

    added another variable

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -3,3 +3,10 @@ window.alert("Hello, World!");
 var A="This is a string";
 
 document.write("Luffy said, \"I\'m going" + " to be King of the Pirates!\"");
+
+var B=" Skinny iced" +" vanilla mocha"
+document.write(B)
+
+var Menu="Burgers", Original="with cheese", Backyard="BBQ", Veggie="lentil patty";
+
+document.write(Backyard);
