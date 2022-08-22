885e30f5a9fbf070b7bac0de9aa13211f54808ea
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-08-22 16:54:55 -0700

    added function call

--- Project2_functions/JS/main.js
+++ Project2_functions/JS/main.js
@@ -3,3 +3,9 @@ function Function(){
   var str2="Also a button"
   document.getElementById("Button_Text").innerHTML=str1+str2;
 }
+
+function myFunction(){
+  var sentence="I could really use"
+  sentence+=" a banana bread";
+  document.getElementById("Concatenate").innerHTML=sentence;
+}
