8d914c66898febea4621acc1450defac31f2fccd
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-08-22 16:09:46 -0700

    created 2 variables and concatenate

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -13,7 +13,11 @@ document.write(Backyard);
 
 document.write(5+6);
 
-function My_First_Function(){
-  var str="This is the button text!";
-  document.getElementById("Button_Text").innerHTML=str;
+function My_First_Function(){ //Defining a function and naming it
+  var str="This is the button text!"; //Defining a variable and giving it
+  document.getElementById("Button_Text").innerHTML=str; //testing commenting
 }
+
+var Sent1="This is the beginning of the string";
+var Sent2=" and this is the end of the string";
+document.write(Sent1+Sent2);
