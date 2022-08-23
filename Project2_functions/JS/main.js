3fca9540e4d4fecd8b26a6a0979e3fcd88bf93a7
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-08-22 19:13:49 -0700

    commented code

--- Project2_functions/JS/main.js
+++ Project2_functions/JS/main.js
@@ -1,11 +1,11 @@
-function Function(){ 
+function Function(){  //created new function
   var str1="This is the button text!"; 
-  var str2="Also a button"
-  document.getElementById("Button_Text").innerHTML=str1+str2;
+  var str2="Also a button" //added second variable
+  document.getElementById("Button_Text").innerHTML=str1+str2; //added both together...is this right?
 }
 
-function myFunction(){
-  var sentence="I could really use";
-  sentence+=" some banana bread."
+function myFunction(){ //Added new function
+  var sentence="I could really use"; //added new variable and defined it
+  sentence+=" some banana bread." //added variable to concatenate
   document.getElementById("Concatenate").innerHTML=sentence;
 }
