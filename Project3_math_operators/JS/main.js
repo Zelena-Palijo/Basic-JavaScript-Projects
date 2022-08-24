749b80d60be5aeb25e8847d3a63937e8f616bc62
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-08-23 21:11:25 -0700

    added subtraction function

--- Project3_math_operators/JS/main.js
+++ Project3_math_operators/JS/main.js
@@ -2,3 +2,8 @@ function addition_Function(){
   var addition=36+28; //function to be done
   document.getElementById("Math").innerHTML="36+28 = "+addition; //make it so it shows the whole add process
 }
+
+function subtraction_Function(){
+  var Subtraction=36-28; //function to be done
+  document.getElementById("Math-2").innerHTML="36-28 ="+Subtraction;
+}
