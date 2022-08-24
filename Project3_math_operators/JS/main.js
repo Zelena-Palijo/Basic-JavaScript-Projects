5a5659c2cc51edaabc5b2edf02e2192d093c7306
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-08-23 21:09:06 -0700

    fixed output showing addition

--- Project3_math_operators/JS/main.js
+++ Project3_math_operators/JS/main.js
@@ -1,4 +1,4 @@
 function addition_Function(){
   var addition=36+28; //function to be done
-  document.getElementById("Math").innerHTML="36+28"+addition; //named Math ID
+  document.getElementById("Math").innerHTML="36+28 = "+addition; //make it so it shows the whole add process
 }
