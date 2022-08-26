ff9bfd30fb194dd2a3fa6c14b017ff9e86f0b68d
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-08-25 21:53:44 -0700

    updated for delete dict assignment

--- Project4_dictionaries/JS/main.js
+++ Project4_dictionaries/JS/main.js
@@ -1,4 +1,4 @@
-function my_Dictionary(){
+function my_Dictionary(){ //created dictionary KVP
   var Hero={
     Species:"Cyborg",
     Color:"Steel",
@@ -7,4 +7,16 @@ function my_Dictionary(){
     Catchphrase:"Master!"
   };
   document.getElementById("Dictionary").innerHTML=Hero.Class;
+}
+
+function my_Dictionary(){
+  var Hero={
+    Species:"Cyborg",
+    Color:"Steel",
+    Class: "S",
+    Age:19,
+    Catchphrase:"Master!"
+  };
+  delete Hero.Age; //delete assignment prompt
+  document.getElementById("Dictionary-1").innerHTML=Hero.Age; //should produce undefined
 }
