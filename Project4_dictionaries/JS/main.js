21e38139ab7001a7322435bd4d10f21a4a3c371e
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-08-25 21:47:29 -0700

    added dictionary

---
+++ Project4_dictionaries/JS/main.js
@@ -0,0 +1,10 @@
+function my_Dictionary(){
+  var Hero={
+    Species:"Cyborg",
+    Color:"Steel",
+    Class: "S",
+    Age:19,
+    Catchphrase:"Master!"
+  };
+  document.getElementById("Dictionary").innerHTML=Hero.Class;
+}
