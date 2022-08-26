0b0af0f5dbad92a9e1299609921a3d5a4a3a5dbc
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-08-25 21:33:52 -0700

    added math method

--- Project3_math_operators/JS/main.js
+++ Project3_math_operators/JS/main.js
@@ -20,25 +20,27 @@ function division(){
 
 function more_Math(){
   var simple_Math=(3+6)*36/2-8;
-  document.getElementById("Math-5").innerHTML="3 plus 6,multiplied by 36, divided in half, and then subtracted by 8 equals "+simple_Math;
+  document.getElementById("Math-5").innerHTML="3 plus 6,multiplied by 36, divided in half, and then subtracted by 8 equals "+simple_Math; //multiple ops
 }
 
 function modulus_Operator(){
   var simple_M=25%6;
-  document.getElementById("Math-6").innerHTML="25 divided by 6 results in a remainder of: "+simple_M;
+  document.getElementById("Math-6").innerHTML="25 divided by 6 results in a remainder of: "+simple_M; //getting remainder
 }
 
 function negation_Operator(){
   var x=1000;
-  document.getElementById("Math-7").innerHTML=-x;
+  document.getElementById("Math-7").innerHTML=-x; //creates the opposite of x
 }
 
 var X=14;
 X++;
-document.write(X);
+document.write(X); //increment
 
 var Y=14;
 Y--;
-document.write(Y);
+document.write(Y); //decrement
 
-window.alert(Math.random());
+window.alert(Math.random()); //generates random number
+
+Math.round(7.8) //math object method should round to 8
