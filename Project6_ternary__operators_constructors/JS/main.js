ad4d8bf012dde1efde76f8c0f48fe02f5c088f45
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-08-27 15:28:46 -0700

    ternary operators voting chall

--- Project6_ternary__operators_constructors/JS/main.js
+++ Project6_ternary__operators_constructors/JS/main.js
@@ -4,3 +4,10 @@ function Ride_Function(){
   Can_ride=(Height<52)?"You are too short":"You are tall enough";
   document.getElementById("Ride").innerHTML=Can_ride+" to ride.";
 }
+
+function Vote_Function(){
+  var Age, Can_vote;
+  Age=document.getElementById("Age").ariaValueMax;
+  Can_vote=(Age<18)?"You are too young":"You are old enough";
+  document.getElementById("Noise").innerHTML=Can_hear+" to vote.";
+}
