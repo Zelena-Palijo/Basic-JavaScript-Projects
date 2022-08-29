897e585ad669f6ef34eca52547a8d218a9f51df6
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-08-28 17:30:18 -0700

    else if

--- Project7_scope_time_function/JS/main.js
+++ Project7_scope_time_function/JS/main.js
@@ -59,3 +59,19 @@ function Age_Function(){
   }
   document.getElementById("How_old_are_you?").innerHTML=Drink;
 }
+
+//else if statements
+function Time_function(){
+  var Time=new Date().getHours();
+  var Reply;
+  if(Time<12==Time>0){
+    Reply="It is morning time!";
+  }
+  else if(Time>=12==Time<18){
+    Reply="It is afternoon.";
+  }
+  else{
+    Reply="It is evening time.";
+  }
+  document.getElementById("Time_of_day").innerHTML=Reply;
+}
