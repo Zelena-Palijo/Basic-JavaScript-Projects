2c6a381f5579cc6074539d89b79da24379526f5a
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-08-28 18:24:15 -0700

    added slice

--- Project8_string_methods/JS/main.js
+++ Project8_string_methods/JS/main.js
@@ -7,3 +7,11 @@ function full_Sentence(){
   var whole_sentence=part_1.concat(part_2,part_3,part_4);
   document.getElementById("Concatenate").innerHTML=whole_sentence;
 }
+
+//slice method
+function slice_Method(){
+  var Sentence="I was born in the rain on the Pontchartrain underneath the Louisiana moon."
+  var Section=Sentence.slice(30,43);
+  document.getElementById("Slice").innerHTML=Section;
+}
+
