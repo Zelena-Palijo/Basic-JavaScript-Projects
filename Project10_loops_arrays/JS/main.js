82c01d2448645b5bb7ee3e8b9a1b51e5b789f0b4
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-09-01 21:24:56 -0700

    added forloop

--- Project10_loops_arrays/JS/main.js
+++ Project10_loops_arrays/JS/main.js
@@ -12,3 +12,14 @@ function Call_Loop(){ //countdown
 let text="Heya heya";
 let length=text.length;
 document.getElementById("demo").innerHTML=length;
+
+//for loop
+var Instruments=["Guitar","Drums","Piano","Bass","Violin","Trumpet","Flute"];
+var Content="";
+var Y;
+function for_Loop(){
+  for (Y=0;Y<Instruments.length;Y++){
+    Content += Instruments[Y]+"<br>";
+  }
+  document.getElementById("List_of_Instruments").innerHTML=Content;
+}
