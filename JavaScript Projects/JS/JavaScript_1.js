8a889d6b58a0ba1cb6a98eca5cec90c5bf75e3f6
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-09-03 17:46:26 -0700

    changed input types and choices

--- JavaScript Projects/JS/JavaScript_1.js
+++ JavaScript Projects/JS/JavaScript_1.js
@@ -1,28 +1,27 @@
-function Color_Function(){
+function Color_Function(){ //named colour but choosing soda flavors
+
+
   var Color_Output;
   var Colors=document.getElementById("Color_Input").value;
-  var Color_String=" is a great color!";
+  var Color_String=" is a great soda choice!";
   switch(Colors){
-    case "Red":
-      Color_Output="Red"+Color_String;
+    case "Cola":
+      Color_Output="Cola"+Color_String;
     break;
-    case "Yellow":
-      Color_Output="Yellow"+Color_String;
+    case "Lemon-lime":
+      Color_Output="Lemon-lime"+Color_String;
     break;
-    case "Green":
-      Color_Output="Green"+Color_String;
+    case "Root beer":
+      Color_Output="Root beer"+Color_String;
     break;
-    case "Blue":
-      Color_Output="Blue"+Color_String;
+    case "Orange":
+      Color_Output="Orange"+Color_String;
     break;
-    case "Pink":
-      Color_Output="Pink"+Color_String;
-    break;
-    case "Purple":
-      Color_Output="Purple"+Color_String;
+    case "Diet":
+      Color_Output="Diet"+Color_String;
     break;
     default:
-      Color_Output="Please enter a color exactly as written on the above list.";
+      Color_Output="Please enter a soda type as written on the above list.";
   }
   document.getElementById("Output").innerHTML=Color_Output;
 }
